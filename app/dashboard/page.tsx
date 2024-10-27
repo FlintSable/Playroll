436b6eba5b58eeec816f49a96e6884d939a19346
FlintSable <nnlaor@gmail.com>
2024-10-26 21:34:50 -0400

    adding dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Component from '@/components/DashboardContent';
+
+export default function Dashboard(){
+  return(
+    <div className="dashboard-container">
+      <DashboardContent />
+    </div>
+  )
+}
